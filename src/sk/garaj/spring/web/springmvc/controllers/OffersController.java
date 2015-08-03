444f3bf64445ac208a7f625c7c3525e764268144
garajm <garaj.michael@gmail.com>
2015-08-03 23:07:36 +0200

    Lesson 64

--- src/sk/garaj/spring/web/springmvc/controllers/OffersController.java
+++ src/sk/garaj/spring/web/springmvc/controllers/OffersController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import sk.garaj.spring.web.springmvc.dao.Offer;
 import sk.garaj.spring.web.springmvc.services.OffersService;
@@ -16,6 +18,12 @@ public class OffersController {
 	@Autowired
 	private OffersService offersService;
 
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String showTest(Model model, @RequestParam("id") String id) {
+		System.out.println("Id is " + id);
+		return "home";
+	}
+	
 	@RequestMapping("/offers")
 	public String showOffers(Model model) {
 		List<Offer> offers = offersService.getCurrent();
