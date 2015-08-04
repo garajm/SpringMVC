d181fa1d6db1ee4f6bf9928f4fe04f20d19b495c
garajm <garaj.michael@gmail.com>
2015-08-04 20:37:03 +0200

    Lesson 70

--- src/sk/garaj/spring/web/springmvc/dao/Offer.java
+++ src/sk/garaj/spring/web/springmvc/dao/Offer.java
@@ -1,15 +1,20 @@
 package sk.garaj.spring.web.springmvc.dao;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Offer {
 	private int id;
 	
-	@Size(min=5, max=100)
+	@Size(min=5, max=100, message="Name must be between 5 and 100 characters")
 	private String name;
 	
-	
+	@NotNull
+	@Pattern(regexp=".*\\@.*\\..*", message="Email address is not valid")
 	private String email;
+	
+	@Size(min=20, max=255, message="Text must be between 20 and 255 characters")
 	private String text;
 	
 	public Offer() {
